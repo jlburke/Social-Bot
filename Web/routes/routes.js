de6f3cea2404e8023d0eff4d897f132156006528
G4lile00 <vitinhomarcelo21@gmail.com>
2021-08-05 15:03:08 -0300

    Routing

---
+++ Web/routes/routes.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var routes = express.Router();
+
+const fs = require("fs");
+
+
+//GET home route
+routes.get('/',  (req, res) =>
+	// read default file
+
+	fs.readFile("/home/g4lile0/Desktop/Social-Bot/Web/src/public/index.ejs", (error, data) => {
+		if (error) {
+			res.send("Error 404");
+		} else {
+			res.write(data);
+			res.end();
+		}
+	})
+
+    );
+
+
+module.exports = routes;
--- Web/src/index.js
+++ Web/src/index.js
@@ -1,10 +1,14 @@
+const fs = require("fs");
+
+const routes = require('../routes/routes');
+
 const express = require("express");
+app = express();
+
 const http = require("http");
 const https = require("https");
-const fs = require("fs");
 const httpPort = 8080;
 const httpsPort = 4433;
-app = express();
 
 var key = fs.readFileSync(
 	"/home/g4lile0/Desktop/Social-Bot/Web/certs/selfsigned.key"
@@ -20,27 +24,15 @@ var credentials = {
 
 app.use(express.static(__dirname + "/public"));
 
-//GET home route
-app.get("/", (_req, res) =>
-	// read default file
-
-	fs.readFile(__dirname + "/public/index.ejs", (error, data) => {
-		if (error) {
-			res.send("Error 404");
-		} else {
-			res.write(data);
-			res.end();
-		}
-	})
-);
+app.use('/', routes);
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(httpPort, () => {
-	console.log("Http server listing on port : " + httpPort);
+	console.log("http://localhost:" + httpPort);
 });
 
 httpsServer.listen(httpsPort, () => {
-	console.log("Https server listing on port : " + httpsPort);
+	console.log("https://localhost:" + httpsPort);
 });
