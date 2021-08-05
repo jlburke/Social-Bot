8f0831ed03555b129e5c6ae23333a477339d127e
G4lile00 <vitinhomarcelo21@gmail.com>
2021-08-04 22:55:27 -0300

    formated

--- Web/src/index.js
+++ Web/src/index.js
@@ -1,47 +1,46 @@
-const express = require('express');
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
+const express = require("express");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const httpPort = 8080;
 const httpsPort = 4433;
-app = express()
+app = express();
 
-var key = fs.readFileSync('/home/g4lile0/Desktop/Social-Bot/Web/certs/selfsigned.key');
-var cert = fs.readFileSync('/home/g4lile0/Desktop/Social-Bot/Web/certs/selfsigned.crt');
+var key = fs.readFileSync(
+	"/home/g4lile0/Desktop/Social-Bot/Web/certs/selfsigned.key"
+);
+var cert = fs.readFileSync(
+	"/home/g4lile0/Desktop/Social-Bot/Web/certs/selfsigned.crt"
+);
 
 var credentials = {
-  key: key,
-  cert: cert
+	key: key,
+	cert: cert,
 };
 
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 //GET home route
-app.get('/', (req, res) => 
+app.get("/", (_req, res) =>
+	// read default file
 
-// read default file 
-
-fs.readFile(__dirname + "/public/index.ejs", (error, data) =>{
-  if(error){
-     res.send('Error 404')
-    }else{
-  res.write(data)
-  res.end()
-     }
-   
-    })
-
+	fs.readFile(__dirname + "/public/index.ejs", (error, data) => {
+		if (error) {
+			res.send("Error 404");
+		} else {
+			res.write(data);
+			res.end();
+		}
+	})
 );
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
-
 httpServer.listen(httpPort, () => {
-  console.log("Http server listing on port : " + httpPort)
+	console.log("Http server listing on port : " + httpPort);
 });
 
 httpsServer.listen(httpsPort, () => {
-  console.log("Https server listing on port : " + httpsPort)
+	console.log("Https server listing on port : " + httpsPort);
 });
