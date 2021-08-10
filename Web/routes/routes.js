480f8e5823945115c48678edda22a1089ee145bb
G4lile00 <vitinhomarcelo21@gmail.com>
2021-08-10 19:53:48 -0300

    Ip log builded and Routing defined

--- Web/routes/routes.js
+++ Web/routes/routes.js
@@ -1,32 +1,67 @@
-var express = require('express');
-var routes = express.Router();
+let express = require("express");
+let routes = express.Router();
 
 const fs = require("fs");
 
+const ip = require("ip");
 
 //GET home route
-routes.get('/',  (req, res) =>
-	// read default file
+routes.get("/", (req, res) =>
+  // read default file
 
-	fs.readFile("/home/g4lile0/Desktop/Social-Bot/Web/src/public/index.ejs", (error, data) => {
-		if (error) {
-			res.send("Error 404");
-		} else {
-			res.write(data);
-			res.end();
-		}
-	})
+  fs.readFile(
+    "/home/g4lile0/Desktop/Social-Bot/Web/src/public/index.ejs",
+    (error, data) => {
+      if (error) {
+        res.send("Error 404");
+      } else {
+        res.write(data);
+        res.end();
+      }
+      //define user IP
+      let ip_raw = req.connection.remoteAddress;
+      let ips = ip_raw.split("::ffff:").pop();
 
-    );
+      //atribute for write Stream
+      let stream = fs.createWriteStream(
+        "/home/g4lile0/Desktop/Social-Bot/Web/logs/ip.txt",
+        { flags: "a" }
+      );
 
-	routes.get('/register',  (req, res) =>
-	fs.readFile("/home/g4lile0/Desktop/Social-Bot/Web/src/public/register.ejs", (error, data) => {
-		if (error) {
-			res.send("Error 404");
-		} else {
-			res.write(data);
-			res.end();
-		}
-	})
-    );
+      // write this ip into a text file
+      stream.once("open", function (fd) {
+        stream.write(`${ips}\n`);
+        stream.end();
+      });
+    }
+  )
+);
+//GET register route
+routes.get("/register", (req, res) =>
+  fs.readFile(
+    "/home/g4lile0/Desktop/Social-Bot/Web/src/public/register.ejs",
+    (error, data) => {
+      if (error) {
+        res.send("Error 404");
+      } else {
+        res.write(data);
+        res.end();
+      }
+    }
+  )
+);
+//GET login route
+routes.get("/login", (req, res) =>
+  fs.readFile(
+    "/home/g4lile0/Desktop/Social-Bot/Web/src/public/login.ejs",
+    (error, data) => {
+      if (error) {
+        res.send("Error 404");
+      } else {
+        res.write(data);
+        res.end();
+      }
+    }
+  )
+);
 module.exports = routes;
--- Web/src/index.js
+++ Web/src/index.js
@@ -1,3 +1,4 @@
+// Libriries import
 const fs = require("fs");
 
 const routes = require("../routes/routes");
@@ -11,31 +12,34 @@ const https = require("https");
 const httpPort = 80;
 const httpsPort = 443;
 
+
+//Certificate define for SSL
 var key = fs.readFileSync(
-  "/home/g4lile0/Desktop/Social-Bot/Web/certs/selfsigned.key"
+  "/home/g4lile0/Desktop/Social-Bot/Web/certs/private.key"
 );
 var cert = fs.readFileSync(
-  "/home/g4lile0/Desktop/Social-Bot/Web/certs/selfsigned.crt"
+  "/home/g4lile0/Desktop/Social-Bot/Web/certs/certificate.crt"
 );
+
 
 var credentials = {
   key: key,
   cert: cert,
 };
 
+//routing
 app.use(express.static(__dirname + "/public"));
 
 app.use("/", routes);
-app.use("/register", routes);
 
-
+//Server Init
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(httpPort, () => {
-  console.log("http://localhost:" + httpPort);
+  console.log("http://socialbots.ddns.net");
 });
 
 httpsServer.listen(httpsPort, () => {
-  console.log("https://localhost:" + httpsPort);
+  console.log("https://socialbots.ddns.net" );
 });
